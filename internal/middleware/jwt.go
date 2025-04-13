21cd1af3815111261cdd9b466d277ac7347d1b8a
Nur Arini <nurarini0302@gmail.com>
2025-04-13 14:56:17 +0700

    feat: implement JWT authentication for user login and registration

---
+++ internal/middleware/jwt.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"time"
+	"os"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateJWT(userID uint) (string, error) {
+	secret := os.Getenv("JWT_SECRET")
+
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(), 
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,52 +1,66 @@
 package user
 
 import (
-    "golang.org/x/crypto/bcrypt"
+	"errors"
+	"github.com/nrrarnn/saldaq-backend-go/internal/middleware"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService interface {
-    Register(name, email, password string) (*User, error)
-    Login(email, password string) (*User, error)
+	Register(name, email, password string) (*User, error)
+	Login(email, password string) (map[string]interface{}, error) 
 }
 
 type userService struct {
-    repo UserRepository
+	repo UserRepository
 }
 
 func NewUserService(repo UserRepository) UserService {
-    return &userService{repo}
+	return &userService{repo}
 }
 
 func (s *userService) Register(name, email, password string) (*User, error) {
-    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-    if err != nil {
-        return nil, err
-    }
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
 
-    user := &User{
-        Name:     name,
-        Email:    email,
-        Password: string(hashedPassword),
-    }
+	user := &User{
+		Name:     name,
+		Email:    email,
+		Password: string(hashedPassword),
+	}
 
-    err = s.repo.Create(user)
-    if err != nil {
-        return nil, err
-    }
+	err = s.repo.Create(user)
+	if err != nil {
+		return nil, err
+	}
 
-    return user, nil
+	return user, nil
 }
 
-func (s *userService) Login(email, password string) (*User, error) {
-    user, err := s.repo.FindByEmail(email)
-    if err != nil {
-        return nil, err
-    }
+func (s *userService) Login(email, password string) (map[string]interface{}, error) {
+	user, err := s.repo.FindByEmail(email)
+	if err != nil {
+		return nil, err
+	}
 
-    err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-    if err != nil {
-        return nil, err
-    }
+	// Verifikasi password
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return nil, errors.New("wrong password")
+	}
 
-    return user, nil
+	// Generate JWT token
+	token, err := middleware.GenerateJWT(user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return map[string]interface{}{
+		"id":    user.ID,
+		"name":  user.Name,
+		"email": user.Email,
+		"token": token,
+	}, nil
 }
